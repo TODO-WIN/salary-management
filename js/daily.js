52004698d0621753745238b872191067c32f1d5a
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-05-02 16:17:25 +0300

    nedal edit css and html

--- js/daily.js
+++ js/daily.js
@@ -14,72 +14,72 @@ const tbody = document.getElementById('tbody');
 
 
 function addBalance(event) {
-    event.preventDefault();
-    let inputBalance = parseInt(document.getElementById('salary').value);
-    //console.log(inputBalance);
-    localStorage.setItem('salary', JSON.stringify(inputBalance));
-    render();
+  event.preventDefault();
+  let inputBalance = parseInt(document.getElementById('salary').value);
+  //console.log(inputBalance);
+  localStorage.setItem('salary', JSON.stringify(inputBalance));
+  render();
 }
 
 function render() {
-    card.innerHTML = '';
-    let pTotal = document.createElement('p');
-    card.appendChild(pTotal);
-    let total = JSON.parse(localStorage.getItem('salary'));
-    if (total) {
-        balance = parseInt(total);
-    }
-    pTotal.textContent = `Current Balance ${balance}`;
-    let saveExpense = JSON.parse(localStorage.getItem('exp'));
-    if (saveExpense) {
-        expenses = parseInt(saveExpense);
-    }
-    let exp = document.createElement('p');
-    card.appendChild(exp);
-    exp.textContent = `Expenses  ${expenses}`;
-    //console.log(expenses);
-    let remain = document.createElement('p');
-    card.appendChild(remain);
-    remaining = balance - expenses;
-    remain.textContent = `Remaining  ${remaining}`;
-    //console.log(remaining);
+  card.innerHTML = '';
+  let pTotal = document.createElement('p');
+  card.appendChild(pTotal);
+  let total = JSON.parse(localStorage.getItem('salary'));
+  if (total) {
+    balance = parseInt(total);
+  }
+  pTotal.textContent = `Current Balance ${balance}`;
+  let saveExpense = JSON.parse(localStorage.getItem('exp'));
+  if (saveExpense) {
+    expenses = parseInt(saveExpense);
+  }
+  let exp = document.createElement('p');
+  card.appendChild(exp);
+  exp.textContent = `Expenses  ${expenses}`;
+  //console.log(expenses);
+  let remain = document.createElement('p');
+  card.appendChild(remain);
+  remaining = balance - expenses;
+  remain.textContent = `Remaining  ${remaining}`;
+  //console.log(remaining);
 
 }
 
 
 function addExpenses(event) {
-    event.preventDefault();
-    let type = document.getElementById('type').value;
-    let amount = parseInt(document.getElementById('amount').value);
-    //console.log(type, amount);
-    new Salary(type, amount);
-    localStorage.setItem('salaryAll', JSON.stringify(Salary.all));
-    tableRender();
-    expenses += amount;
-    //console.log("exp=" + expenses);
-    localStorage.setItem('exp', JSON.stringify(expenses));
-    render();
-    tableRender();
+  event.preventDefault();
+  let type = document.getElementById('type').value;
+  let amount = parseInt(document.getElementById('amount').value);
+  //console.log(type, amount);
+  new Salary(type, amount);
+  localStorage.setItem('salaryAll', JSON.stringify(Salary.all));
+  tableRender();
+  expenses += amount;
+  //console.log("exp=" + expenses);
+  localStorage.setItem('exp', JSON.stringify(expenses));
+  render();
+  tableRender();
 
 }
 
 function tableRender() {
-    tbody.innerHTML = '';
-    let data = JSON.parse(localStorage.getItem('salaryAll'));
-    if (data) {
-        for (let i = 0; i < data.length; i++) {
-            const tRow = document.createElement('tr');
-            tbody.appendChild(tRow);
-            const td1 = document.createElement('td');
-            tRow.appendChild(td1);
-            td1.textContent = data[i].name;
-            //console.log(data[i].name);
-            const td2 = document.createElement('td');
-            tRow.appendChild(td2);
-            td2.textContent = data[i].amount;
-            //console.log(data[i].amount);
-        }
+  tbody.innerHTML = '';
+  let data = JSON.parse(localStorage.getItem('salaryAll'));
+  if (data) {
+    for (let i = 0; i < data.length; i++) {
+      const tRow = document.createElement('tr');
+      tbody.appendChild(tRow);
+      const td1 = document.createElement('td');
+      tRow.appendChild(td1);
+      td1.textContent = data[i].name;
+      //console.log(data[i].name);
+      const td2 = document.createElement('td');
+      tRow.appendChild(td2);
+      td2.textContent = data[i].amount;
+      //console.log(data[i].amount);
     }
+  }
 }
 render();
 tableRender();
